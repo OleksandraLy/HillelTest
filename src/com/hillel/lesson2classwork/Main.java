ef6451a7e995f0d80ccc2fa780148997e016ce5f
OleksandraLy <ol.lystopad@gmail.com>
2021-03-06 10:40:02 +0200

    lesson 2: test commit

---
+++ src/com/hillel/lesson2classwork/Main.java
@@ -0,0 +1,9 @@
+package com.hillel.lesson2classwork;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello lesson 2");
+	// write your code here
+    }
+}
