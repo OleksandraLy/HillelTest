e3562c0ac6489fa290097b4ab7448f2bf6dbb4e5
OleksandraLy <ol.lystopad@gmail.com>
2021-03-14 17:55:15 +0200

    lesson 3: homework task 4 commit

---
+++ src/com/hillel/lesson3homework/Math.java
@@ -0,0 +1,24 @@
+package com.hillel.lesson3homework;
+
+public class Math {
+    public static void main(String[] args) {
+        double squareDigit = java.lang.Math.sqrt(144);
+        System.out.println(squareDigit);
+        int absoluteDigit = java.lang.Math.abs(-31);
+        System.out.println(absoluteDigit);
+        int minimalDigit = java.lang.Math.min(12, 44);
+        System.out.println(minimalDigit);
+        int maximumDigit = java.lang.Math.max(302, 545);
+        System.out.println(maximumDigit);
+        double randomDigit = java.lang.Math.random();
+        System.out.println(randomDigit);
+        double roundDigit = java.lang.Math.round(5.214);
+        System.out.println(roundDigit);
+        int incrementDigit = java.lang.Math.incrementExact(3);
+        System.out.println(incrementDigit);
+        double floorDigit = java.lang.Math.floor(8.1152);
+        System.out.println(floorDigit);
+
+    }
+
+}
