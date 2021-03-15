ff08cb106031a9e7c18a2b46c63f6818916f793c
OleksandraLy <ol.lystopad@gmail.com>
2021-03-15 19:01:10 +0200

    lesson 4: homework task 1 commit

---
+++ src/com/hillel/lesson4homework/Lesson4Homework.java
@@ -0,0 +1,17 @@
+package com.hillel.lesson4homework;
+
+public class Lesson4Homework {
+        public static void main(String[] args) {
+                int result = task1();
+                }
+        public static int task1() {
+                int a = 0;
+                while (a<10) {
+                        a++;
+                        System.out.println(a);
+                }
+                return a;
+        }
+
+        }
+
