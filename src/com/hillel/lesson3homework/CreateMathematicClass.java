ed1b7de90043f014da33a13d09deb5f62a16f426
OleksandraLy <ol.lystopad@gmail.com>
2021-03-16 14:59:17 +0200

    lesson 3: homework task 6 commit

---
+++ src/com/hillel/lesson3homework/CreateMathematicClass.java
@@ -0,0 +1,20 @@
+package com.hillel.lesson3homework;
+
+public class CreateMathematicClass {
+    /*public static void main(String[] args) {
+
+    }
+    public int summary(){
+    }
+    public static void subtraction() {
+
+    }
+    public static void multiplication(double x) {
+
+    }
+    public double division(double y){
+
+    }*/
+
+
+}
