62ecce5dd34fa9c2731e390682f4e1d6bb729041
OleksandraLy <ol.lystopad@gmail.com>
2021-04-08 16:07:24 +0300

    lesson 8: hometask changed 3 commit

--- src/com/hillel/lesson8homework/House3.java
+++ src/com/hillel/lesson8homework/House3.java
@@ -1,9 +1,60 @@
 package com.hillel.lesson8homework;
 
 public class House3 {
-    Mirror theMirror;
-    Bed theBed;
-    Chair theChair;
-    Dresser theDresser;
-    Table theTable;
+    private Mirror theMirror;
+    private Bed theBed;
+    private Chair theChair;
+    private Dresser theDresser;
+    private Table theTable;
+
+    public House3 (Mirror theMirror) {
+        this("wooden","triangular");
+        this.theMirror=theMirror;
+    }
+
+    public House3(Bed theBed, Chair theChair) {
+        this.theBed = theBed;
+        this.theChair = theChair;
+    }
+
+
+    public House3 (Mirror theMirror, Bed theBed, Chair theChair, Dresser theDresser, Table theTable){
+        this.theMirror=theMirror;
+        this.theBed = theBed;
+        this.theChair=theChair;
+        this.theDresser=theDresser;
+        this.theTable=theTable;
+    }
+
+    public Mirror getTheMirror() {
+        return theMirror;
+    }
+
+    public void setTheMirror(Mirror theMirror) {
+        this.theMirror = theMirror;
+    }
+    public Bed getTheBed(){
+        return theBed;
+    }
+    public void setTheBed(Bed theBed) {
+        this.theBed=theBed;
+    }
+    public Chair getTheChair() {
+        return theChair;
+    }
+    public void setTheChair() {
+        this.theChair=theChair;
+    }
+    public Dresser getTheDresser() {
+        return theDresser;
+    }
+    public void setTheDresser() {
+        this.theDresser=theDresser;
+    }
+    public Table getTheTable() {
+        return theTable;
+    }
+    public Table setTheTable() {
+        this.theTable=theTable;
+    }
 }
