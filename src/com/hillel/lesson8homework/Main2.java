38b32c8b1e936c7cd466489e96ccb80f0a044e70
OleksandraLy <ol.lystopad@gmail.com>
2021-04-08 14:03:41 +0300

    lesson 8: hometask changed 2 commit

---
+++ src/com/hillel/lesson8homework/Main2.java
@@ -0,0 +1,9 @@
+package com.hillel.lesson8homework;
+
+public class Main2 {
+    Mirror theMirror = new Mirror(3, "oval");
+    Chair theChair = new Chair("tulip","triangular");
+    Bed theBed= new Bed("double","wooden");
+    Table theTable=new Table("square",4);
+    Dresser theDresser = new Dresser(5,"soft oak");
+}
